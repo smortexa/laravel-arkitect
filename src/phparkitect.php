2c4fe61112bd864aec80b7f5a358ce1674509350
SMorteza Ebadi <47121888+smortexa@users.noreply.github.com>
2022-08-19 05:59:29 +0430

    Add rule loader (#13)

--- src/phparkitect.php
+++ src/phparkitect.php
@@ -3,22 +3,10 @@
 declare(strict_types=1);
 
 use Arkitect\CLI\Config;
-use Illuminate\Support\Arr;
-use Mortexa\LaravelArkitect\CreateApplication;
+use Mortexa\LaravelArkitect\RuleLoader;
 
 return static function (Config $config): void {
-    $app = CreateApplication::app();
-
-    $package_config = $app->make('config')['arkitect'];
-
-    $rules = [];
-
-    $rules = array_merge($rules, Arr::flatten($package_config['rules']));
-
-    foreach (glob($app->basePath('tests/Architecture/*.php')) as $file) {
-        $class = basename($file, '.php');
-        $rules[] = 'Tests\\Architecture\\'.$class;
-    }
+    $rules = RuleLoader::all();
 
     foreach ($rules as $rule) {
         if ($rule::directoryExists()) {
---
+++ tests/Feature/RuleLoaderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Mortexa\LaravelArkitect\RuleLoader;
+use function PHPUnit\Framework\assertEqualsCanonicalizing;
+
+it('loads user\'s rules', function () {
+    $fooClass = base_path('tests/Architecture/Foo.php');
+    $barClass = base_path('tests/Architecture/Bar.php');
+
+    if (File::exists($fooClass)) {
+        unlink($fooClass);
+    }
+
+    if (File::exists($barClass)) {
+        unlink($barClass);
+    }
+
+    Artisan::call('make:arkitect Foo');
+    Artisan::call('make:arkitect Bar');
+
+    $rules = RuleLoader::user();
+
+    assertEqualsCanonicalizing(['Tests\\Architecture\\Foo', 'Tests\\Architecture\\Bar'], $rules);
+});
